7e4191c35eadc225563053c7af2c9d24a9a8c1c5
Steven Harradine <stevenharradine@gmail.com>
2016-03-22 11:32:15 -0400

    add standalone server

---
+++ server.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+var fs = require('fs');
+var PORT = 8080;
+
+var server = http.createServer(function (req, res){
+    dispatcher.dispatch(req, res);
+});
+
+server.listen(PORT, '0.0.0.0', function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    read ('index.html', function (data) {
+        res.end(data);
+    }, function (error) {
+        console.log (error);
+    })
+});
+
+dispatcher.onGet("/functionality.js", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+
+    read ('functionality.js', function (data) {
+        res.end(data);
+    }, function (error) {
+        console.log (error);
+    })
+})
+
+dispatcher.onGet("/styles.css", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/css'});
+
+    read ('styles.css', function (data) {
+        res.end(data);
+    }, function (error) {
+        console.log (error);
+    })
+})
+
+function read (filename, success_callback, error_callback) {
+    fs.readFile(filename, 'utf8', function (err,data) {
+        if (err) {
+            return error_callback (err);
+        }
+        
+        return success_callback (data);
+    });
+}
